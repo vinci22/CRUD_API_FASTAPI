ac4148e6cf8a050c0e02cc99da0b91120fee3a49
vinci22 <dariojose22@outlook.com>
2022-02-17 19:09:10 -0500

    new changes

--- Routes/route_user.py
+++ Routes/route_user.py
@@ -1,6 +1,6 @@
 
 
-from select import select
+
 from fastapi import APIRouter,Request
 from sqlalchemy import delete
 from cryptography.fernet import Fernet
@@ -9,6 +9,7 @@ from Models.UserModel import users
 from Schema.UserSchema import UserSchema 
 
 user_route = APIRouter()
+
 """
 Definicion de endpoint para users
 
@@ -24,20 +25,19 @@ async def CreateUser(data:UserSchema,req:Request):
 
 @user_route.put("/delete/{id}")
 async def DeleteUser(id: int,status:bool)->str:
-    
-    query=conn.execute(users.update().where(users.c.id == id).values({"status":status}))
+    deleteQuery=conn.execute(users.update().where(users.c.id == id).values({"status":status}))
     user_delete=conn.execute(users.select(users.c.Name).where(users.c.id == id))
     return "eliminado"
 
 @user_route.put("/update/{id}")
 async def UpdateUser(id:int)->str:
-    
-    pass
+    updateQuery=conn.execute(users.update().where(users.c.id == id).values())
+    return 'actualizado'
 
 @user_route.get("/user/{id}")
 async def GetUser(id:int)->str:
-    UsersQuery=conn.execute(users.select().where(users.c.id==id)).fetchall()
-    return UsersQuery  
+    getQuery=conn.execute(users.select().where(users.c.id==id)).fetchall()
+    return getQuery  
 
 @user_route.get("/users")
 async def GetUsers()->str:
